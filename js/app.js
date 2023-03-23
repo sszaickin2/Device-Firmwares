4a630bd0e3c2bee75a595de6e9b3e61a3f8eb10a
serge <sergey@zaickin.ru>
2023-03-23 18:10:17 +0300

    update-23.03.2023-2

--- js/app.js
+++ js/app.js
@@ -259,13 +259,16 @@
             }
         }
     }
-    const path = window.location.pathname;
-    const page = path.split("/").pop();
-    const menuLink = document.querySelectorAll(".menu__link");
-    for (let i = 0; i < menuLink.length; i++) {
-        let menuId = menuLink[i].dataset.link;
-        if (page === menuId) menuLink[i].className += " menu__act";
+    function highlightActiveMenuItem() {
+        const currentUrl = window.location.href;
+        const menuLinks = document.querySelectorAll("#menu a");
+        menuLinks.forEach((link => {
+            const linkUrl = link.href;
+            if (currentUrl.includes(linkUrl)) link.parentNode.classList.add("active"); else link.parentNode.classList.remove("active");
+        }));
     }
+    window.addEventListener("load", highlightActiveMenuItem);
+    window.addEventListener("hashchange", highlightActiveMenuItem);
     const contentTabs = document.querySelectorAll(".body-tabs__content");
     if (contentTabs.length > 0) contentTabs.forEach((element => {
         element.addEventListener("click", (event => {
